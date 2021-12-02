18e3cb8b87ea3614dc73b02a2c557297262706bb
Sameer Haque <sameer.haque@cgic.cooperators.ca>
2021-12-02 09:36:32 -0500

    assets

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+import Index from "./index.svelte";
+import Error from "./__error.svelte";
+
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css" />
+</svelte:head>
+
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>

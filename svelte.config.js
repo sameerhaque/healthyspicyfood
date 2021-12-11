c24c0073a4bc3be80449d99426f398d1b96d9b7f
Sameer Haque <sameer.haque@cgic.cooperators.ca>
2021-12-11 09:58:05 -0500

    netlify function

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+const netlify = require('@sveltejs/adapter-netlify')
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: netlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
